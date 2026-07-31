6896bdf709daaac9a9f47a254b8f65d0586e5b80
Priyanshi Nigam <49412738+Priyanshi-99@users.noreply.github.com>
2026-07-31 21:45:35 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,84 @@
+class LRUCache {
+     
+    class Node{
+        int val;
+        int key;
+        Node prev;
+        Node next;
+        public  Node(int key,int val){
+            this.val=val;
+            this.key=key;
+            
+        }
+    }
+   
+       HashMap<Integer,Node> hm=new HashMap<>();
+       Node head;
+       Node tail;
+       int capacity_;
+
+    public LRUCache(int capacity) {
+        
+        this.capacity_=capacity;
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next=tail;
+        tail.prev=head;
+
+         
+        
+    }
+    
+    public int get(int key) {
+        if(!hm.containsKey(key)){
+            return -1;
+        }
+        Node node=hm.get(key);
+        remove(node);
+        insertafterhead(node);
+        return node.val;
+        
+    }
+    
+    public void put(int key, int value) {
+        if(hm.containsKey(key)){
+            Node node=hm.get(key);
+            node.val=value;
+            remove(node);
+            insertafterhead(node);
+            return;
+        }
+        Node node=new Node(key,value);
+        hm.put(key,node);
+        insertafterhead(node);
+
+        if(hm.size()>capacity_){
+            Node temp=tail.prev;
+           
+            remove(temp);
+             hm.remove(temp.key);
+
+        }
+
+           
+        }
+        void remove(Node node){
+            node.prev.next=node.next;
+            node.next.prev=node.prev;
+        }
+        void insertafterhead(Node node){
+            Node temp=head.next;
+            head.next=node;
+            node.prev=head;
+            node.next=temp;
+            temp.prev=node;
+        // Node temp = head.next;
+        // head.next = node;
+        // node.prev = head;
+        // node.next = temp;
+        // temp.prev = node;
+        
+    
+        
+    }
+}
