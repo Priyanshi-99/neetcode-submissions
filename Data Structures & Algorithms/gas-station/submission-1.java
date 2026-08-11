b9177ed56abe663e91cb1fca0557c7b8f17813cf
Priyanshi Nigam <49412738+Priyanshi-99@users.noreply.github.com>
2026-08-11 21:58:48 +0530

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gassum=0;
+        int costsum=0;
+        for(int i=0;i<gas.length;i++){
+            gassum+=gas[i];
+            costsum+=cost[i];
+
+        }
+        if(gassum<costsum) return -1;
+        int total=0;
+        int res=0;
+        for(int i=0;i<gas.length;i++){
+            total+=gas[i]-cost[i];
+            if(total<0){
+                res=i+1;
+                total=0;
+                
+            }
+        }
+        return res;
+    }
+}
