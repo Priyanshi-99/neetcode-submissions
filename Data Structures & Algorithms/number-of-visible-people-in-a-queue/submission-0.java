f1315d5689c1157209d6961298d0a6692a59a330
Priyanshi Nigam <49412738+Priyanshi-99@users.noreply.github.com>
2026-08-27 13:55:28 +0530

    Add: number-of-visible-people-in-a-queue - submission-0

---
+++ Data Structures & Algorithms/number-of-visible-people-in-a-queue/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        Deque<Integer> stack=new ArrayDeque<>();
+
+        int[] ans=new int[heights.length];
+        for(int i=heights.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<heights[i]){
+                stack.pop();
+                ans[i]++;
+            }
+            if(!stack.isEmpty()){
+                ans[i]++;
+            }
+            stack.push(heights[i]);
+        }
+        return ans;
+    }
+}
